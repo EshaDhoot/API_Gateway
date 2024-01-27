9f1314600395c3457a89e99e7b363df899cdbffd
Esha <maheshwariesha11@gmail.com>
2024-01-27 15:13:55 +0530

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 const app = express();
 
-const  { PORT} = require('./serverConfig')
+const   PORT = 3005;
 
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000,
